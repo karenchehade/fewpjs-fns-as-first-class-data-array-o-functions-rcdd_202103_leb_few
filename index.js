d526075c2e845f36e804af4a535611df408f9e58
karen chehade <karen_chehade@hotmail.com>
2021-04-14 09:40:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ function unleashDog(dogName, dogBreed) {
   console.log(str);
   return str;
 }
-const routine =[wakeDog(dogName, dogBreed),leashDog(dogName, dogBreed),walkToPark(dogName, dogBreed)
-,throwFrisbee(dogName, dogBreed),walkHome(dogName, dogBreed),unleashDog(dogName, dogBreed)];
+const routine =[wakeDog(),leashDog(),walkToPark()
+,throwFrisbee(),walkHome(),unleashDog()];
 function exerciseDog(dogName, dogBreed) {
   const result = [];
   for(i=0 ;i< routine.length;i++){
