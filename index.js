2951923db5ea9a18da0d2ada4f9327a6ad70c4e2
karen chehade <karen_chehade@hotmail.com>
2021-04-14 09:26:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 function wakeDog(dogName, dogBreed) {
-  let str=`Wake ${dogName} the ${dogBreed}`;
+  const str=`Wake ${dogName} the ${dogBreed}`;
   console.log(str);
   return str;
 }
 function leashDog(dogName, dogBreed) {
-    let str=`leash ${dogName} the ${dogBreed}`;
+  const str=`leash ${dogName} the ${dogBreed}`;
   console.log(str);
-    return str;
+  return str;
 }
 function walkToPark(dogName, dogBreed) {
-    let str=`Walk to the park with ${dogName} the ${dogBreed}`;
+  const str=`Walk to the park with ${dogName} the ${dogBreed}`;
   console.log(str);
-    return str;
+  return str;
 }
 function throwFrisbee(dogName, dogBreed) {
-    let str=`Throw the frisbee for ${dogName} the ${dogBreed}`;
+  const str=`Throw the frisbee for ${dogName} the ${dogBreed}`;
   console.log(str);
-    return str;
+  return str;
 }
 function walkHome(dogName, dogBreed) {
-    let str=`Walk home with ${dogName} the ${dogBreed}`;
+  const str=`Walk home with ${dogName} the ${dogBreed}`;
   console.log(str);
-    return str;
+  return str;
 }
 function unleashDog(dogName, dogBreed) {
-  let str=`Unleash ${dogName} the ${dogBreed}`;
+  const str=`Unleash ${dogName} the ${dogBreed}`;
   console.log(str);
-    return str;
+  return str;
 }
+const routine =[]
