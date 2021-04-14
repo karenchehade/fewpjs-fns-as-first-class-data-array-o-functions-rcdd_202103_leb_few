9d6f328d4bb36bfe14218257631cfbdf22c1c410
karen chehade <karen_chehade@hotmail.com>
2021-04-14 09:21:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
+function leashDog(dogName, dogBreed) {
+  console.log(`leash ${dogName} the ${dogBreed}`);
+}
+function walkToPark(dogName, dogBreed) {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+}
