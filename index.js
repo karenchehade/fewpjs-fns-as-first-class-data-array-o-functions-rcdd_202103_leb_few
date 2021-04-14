0b763fcbe225614885b9e2e667f6b1235d6b23cc
karen chehade <karen_chehade@hotmail.com>
2021-04-14 09:25:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  let str=`Wake ${dogName} the ${dogBreed}`;
+  console.log(str);
+  return str;
 }
 function leashDog(dogName, dogBreed) {
-  console.log(`leash ${dogName} the ${dogBreed}`);
+    let str=`leash ${dogName} the ${dogBreed}`;
+  console.log(str);
+    return str;
 }
 function walkToPark(dogName, dogBreed) {
-  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    let str=`Walk to the park with ${dogName} the ${dogBreed}`;
+  console.log(str);
+    return str;
 }
 function throwFrisbee(dogName, dogBreed) {
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    let str=`Throw the frisbee for ${dogName} the ${dogBreed}`;
+  console.log(str);
+    return str;
 }
-function wakeDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+function walkHome(dogName, dogBreed) {
+    let str=`Walk home with ${dogName} the ${dogBreed}`;
+  console.log(str);
+    return str;
+}
+function unleashDog(dogName, dogBreed) {
+  let str=`Unleash ${dogName} the ${dogBreed}`;
+  console.log(str);
+    return str;
 }
